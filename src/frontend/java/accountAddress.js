004fc6f1851a2a093bc6b4e3473ec84610a41647
Joost-Sundae <joost.delaere@sundae.finance>
2023-10-12 18:06:17 +0200

    Removed unwanted files

--- src/frontend/java/accountAddress.js
+++
@@ -1,6 +0,0 @@
-export let accountAddress;
-// export let rdt;
-
-export function setAccountAddress(address) {
-  accountAddress = address;
-}
--- src/frontend/java/information.js
+++
@@ -1,8 +0,0 @@
-// JavaScript event listener example
-const infoIcon = document.querySelector('.info-icon');
-const tooltipElement = document.querySelector('.info-icon::after');
-
-infoIcon.addEventListener('mouseover', () => {
-  const tooltipContent = 'Dynamic tooltip content';
-  tooltipElement.textContent = tooltipContent;
-});
